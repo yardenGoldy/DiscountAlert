aa8214d07ebcc202c17f82dcd5ab9e464867f832
Unknown <yarden.goldy@gmail.com>
2020-08-15 22:27:18 +0300

    refactor retry module

--- RetryMechanism/GERetryMechanism.cs
+++ RetryMechanism/GERetryMechanism.cs
@@ -4,7 +4,8 @@ using DiscountAlert.Shared;
 
 namespace DiscountAlert.RetryMechanism
 {
-    public class GERetryMechanism : IGERetryMechanism
+    
+public class GERetryMechanism : IGERetryMechanism
     {
         public IGEExceptionHandler _exceptionHandler { get; set; }
         public GERetryMechanism(IGEExceptionHandler exceptionHandler)
@@ -12,12 +13,29 @@ namespace DiscountAlert.RetryMechanism
             _exceptionHandler = exceptionHandler;
         }
 
-        public TResult InvokeFunc<TResult>(Delegate f, int tries, params object[] args)
+        public TResult InvokeFunc<TResult>(ref object targetInstance, string methodName, int tries, params object[] args)
         {
-            return InvokeFunc<TResult>(f, tries, 100, args);
+            return InvokeFunc<object, TResult>(ref targetInstance, methodName, tries, 100, args);
+        }
+        public TResult InvokeFunc<TTarget, TResult>(ref TTarget targetInstance, string methodName, int tries, params object[] args)
+        {
+            return InvokeFunc<TTarget, TResult>(ref targetInstance, methodName, tries, 100, args);
+        }
+        public TResult InvokeFunc<TResult>(Delegate f, string methodName, int tries, params object[] args)
+        {
+            var target = f.Target;
+            return InvokeFunc<object, TResult>(ref target, methodName, tries, 100, args);
+        }
+        public bool InvokeAction(Delegate f, int tries, params object[] args)
+        {
+            return InvokeAction(f, tries, 100, args);
+        }
+        public bool InvokeAction<TTarget>(Delegate f, int tries, params object[] args)
+        {
+            return InvokeAction(f, tries, 100, args);
         }
 
-        private TResult InvokeFunc<TResult>(Delegate f, int tries, int delay, params object[] args)
+        private TResult InvokeFunc<TTarget, TResult>(ref TTarget targetInstance, string methodName, int tries, int delay, params object[] args)
         {
             // Assign default value for generic type.
             TResult result = default(TResult);
@@ -26,8 +44,16 @@ namespace DiscountAlert.RetryMechanism
             {
                 try
                 {
-                    var resultAsObject = f.DynamicInvoke(args);
-                    result = (TResult)Convert.ChangeType(resultAsObject, typeof(TResult));
+                    //                 var result = f.DynamicInvoke(args);
+                    // return (TResult)Convert.ChangeType(result, typeof(TResult));
+                    // var targetType = ((TResult)f.Target).GetType();
+
+                    // var targetMethod = targetType.GetMethod(f.Method.Name);
+                    // var ddasdsd = targetMethod.Invoke(f.Target, new object[] { args[0] });
+                    // var cast = (TResult)f.Target;
+                    // Activator.CreateInstance(cast.GetType());
+                    var method = targetInstance.GetType().GetMethod(methodName);
+                    result = (TResult)method.Invoke(targetInstance, args);
                     break;
                 }
                 catch (Exception ex)
@@ -49,11 +75,6 @@ namespace DiscountAlert.RetryMechanism
             return result;
         }
 
-        public bool InvokeAction(Delegate f, int tries, params object[] args)
-        {
-            return InvokeAction(f, tries, 100, args);
-        }
-
         private bool InvokeAction(Delegate f, int tries, int delay, params object[] args)
         {
             // Assign default value for generic type.
@@ -63,14 +84,19 @@ namespace DiscountAlert.RetryMechanism
             {
                 try
                 {
-                    var resultAsObject = f.DynamicInvoke(args);
+                    var method = f.Method;
+                    method.Invoke(f.Target, args);
                     invokedSuccessfully = true;
                     break;
                 }
                 catch (Exception ex)
                 {
-                    _exceptionHandler.ExceptionHandler(ex);
+                    var knownError = _exceptionHandler.ExceptionHandler(ex);
 
+                    if (knownError != null)
+                    {
+                        return true;
+                    }
 
                     // todo: Throw error if retry has failed.
                     if (attemptNumber == tries)
--- Shared/IGERetryMechanism.cs
+++ Shared/IGERetryMechanism.cs
@@ -4,7 +4,10 @@ namespace DiscountAlert.Shared
 {
     public interface IGERetryMechanism
     {
-        TResult InvokeFunc<TResult>(Delegate f, int retryNumber, params object[] args);
-        bool InvokeAction(Delegate f, int retryNumber, params object[] args);
+        TResult InvokeFunc<TResult>(Delegate f, string methodName, int tries, params object[] args);
+        TResult InvokeFunc<TResult>(ref object targetInstance, string methodName, int tries, params object[] args);
+        TResult InvokeFunc<TTarget, TResult>(ref TTarget targetInstance, string methodName, int tries, params object[] args);
+        bool InvokeAction(Delegate f, int tries, params object[] args);
+        bool InvokeAction<TTarget>(Delegate f, int tries, params object[] args);
     }
 }
