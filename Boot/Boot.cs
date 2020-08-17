8daa2853be2ce15baebb6f29023cc8443a2890f9
Halevy87 <64663805+Halevy87@users.noreply.github.com>
2020-08-17 20:18:04 +0300

    start add boot

---
+++ Boot/Boot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Boot
+{
+    public class Boot
+    {
+        private static readonly TelegramBotClient bot = new TelegramBotClient("1222193869:AAH4QQJuC2IUJ-0HRxCnBQ98URRNGreBFLIs");
+
+        public Boot(){
+            bot.OnMessage += Csharpcornerbotmessage;  
+            bot.StartReceiving();           
+        }
+
+        private void Csharpcornerbotmessage(object sender, MessageEventArgs e){
+
+        }
+         
+         ~Boot() {
+            bot.StopReciving();
+         }
+    }
+}
